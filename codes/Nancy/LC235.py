25494c8fb1e3de440524ee8e4e2b6b3ce7aa50c9
Nancy Yu <nancyyu.17@gmail.com>
2019-06-17 00:23:55 -0700

    added June's Week2 Day2

---
+++ codes/Nancy/LC235.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        iterative
+        """
+        
+        node = root
+        
+        while node:
+            if p.val > node.val and q.val > node.val:
+                node = node.right
+            elif p.val < node.val and q.val < node.val:
+                node = node.left
+            else:
+                return node
