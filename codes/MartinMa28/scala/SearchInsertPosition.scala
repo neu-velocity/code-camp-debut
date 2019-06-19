9023bdd50a6dc4e1cb29990c5574e967f47942c3
Martin <1135485226@qq.com>
2019-06-20 00:16:03 +0800

    added a scala solution

---
+++ codes/MartinMa28/scala/SearchInsertPosition.scala
@@ -0,0 +1,20 @@
+package week3
+import util.control.Breaks._
+
+object Solution {
+  def searchInsert(nums: Array[Int], target: Int): Int = {
+    for (i <- 0 until nums.length) {
+      if (nums(i) >= target) return i
+    }
+
+    return nums.length
+  }
+}
+
+object SearchInsertPosition {
+  def main(args: Array[String]): Unit = {
+    var i = Solution.searchInsert(Array(1, 3, 5, 6), 2)
+    println(i)
+  }
+
+}
